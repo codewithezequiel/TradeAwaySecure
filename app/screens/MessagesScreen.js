d492dc862db24cca956a12345473e16d3ee73e56
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-22 21:29:31 -0700

    Use hook to delete message via handleDelete func.

---
+++ app/screens/MessagesScreen.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { FlatList, StyleSheet, View } from "react-native";
+
+import Screen from "../components/Screen";
+import ListItem from "../components/ListItem";
+import ListItemSeparator from "../components/ListItemSeparator";
+import ListItemDeleteAction from "../components/ListItemDeleteAction";
+
+const initialMessages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "D1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "D2",
+    image: require("../assets/mosh.jpg"),
+  },
+];
+
+function MessagesScreen(props) {
+  // With the addition of hooks in react, now, function components have all the
+  // capabalities that we have in class components. To declare a state variable here,
+  // we are going to use a hook. A hook is just a function that allows us to
+  // hook into react features in function components.
+
+  const [messages, setMessages] = useState(initialMessages);
+  const handleDelete = (message) => {
+    // Delete the message from messages
+    const newMessages = messages.filter((m) => m.id !== message.id);
+    setMessages(newMessages);
+
+    // Call the server (Connecting our app to the backend)
+  };
+
+  return (
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subTitle={item.description}
+            image={item.image}
+            onPress={() => console.log("Message Selected", item)}
+            renderRightActions={() => (
+              <ListItemDeleteAction onPress={() => handleDelete(item)} />
+            )}
+          />
+        )}
+        ItemSeparatorComponent={ListItemSeparator}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default MessagesScreen;
