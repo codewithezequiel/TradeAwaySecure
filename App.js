185e74f0718f8b76f4233ae190a79a25986fb41b
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-29 12:13:10 -0700

    Build and test my own custom picker item.

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import React, { useState } from "react";
 import { Switch } from "react-native";
+import AppPicker from "./app/components/AppPicker";
+import AppTextInput from "./app/components/AppTextInput";
 
 import Screen from "./app/components/Screen";
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
+  const categories = [
+    { label: "Furniture", value: 1 },
+    { label: "Clothing", value: 2 },
+    { label: "Supplies", value: 3 },
+  ];
+
+  const [category, setCategory] = useState(categories[0]);
   return (
     <Screen>
-      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+      <AppPicker
+        icon="apps"
+        placeholder="Category"
+        items={categories}
+        selectedItem={category}
+        onSelectItem={(item) => setCategory(item)}
+      />
+      <AppTextInput icon="email" placeholder="Email" />
     </Screen>
   );
 }
