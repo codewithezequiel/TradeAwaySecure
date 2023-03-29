345a306be556322882812dc831da8b78db0d8649
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-29 08:37:57 -0700

    Built the listing screens component

--- App.js
+++ App.js
@@ -3,15 +3,11 @@ import React from "react";
 import Screen from "./app/components/Screen";
 import Icon from "./app/components/Icon";
 import ListItem from "./app/components/ListItem";
+import AccountScreen from "./app/screens/AccountScreen";
+import Footer from "./app/screens/Footer";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+import ListingScreens from "./app/screens/ListingScreens";
 
 export default function App() {
-  return (
-    <Screen>
-      <ListItem
-        ImageComponent={<Icon name="email" />}
-        title="My Title"
-        
-      />
-    </Screen>
-  );
+  return <ListingScreens />;
 }
---
+++ app/screens/ListingScreens.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { FlatList, StyleSheet } from "react-native";
+
+import Card from "../components/Card";
+import Screen from "../components/Screen";
+import colors from "../config/colors";
+
+function ListingScreens(props) {
+  //temporary solution: array of objects
+  // when we connect the app the backend, I am going to retrieve the listings
+  // from the server
+  const listings = [
+    {
+      id: 1,
+      title: "Red jacket for sale",
+      price: 100,
+      image: require("../assets/jacket.jpg"),
+    },
+    {
+      id: 2,
+      title: "Couch in great condition",
+      price: 1001,
+      image: require("../assets/couch.jpg"),
+    },
+  ];
+
+  return (
+    <Screen style={styles.screen}>
+      <FlatList
+        data={listings}
+        keyExtractor={(listing) => listing.id.toString()}
+        renderItem={({ item }) => (
+          <Card
+            title={item.title}
+            subTitle={"$" + item.price}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    padding: 20,
+    backgroundColor: colors.light,
+  },
+});
+
+export default ListingScreens;
