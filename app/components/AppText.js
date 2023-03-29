d270b00d19e6796bb01fc1e862d640f25df1001d
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-29 10:18:12 -0700

    Extract and refactor code to make it maintainable.

--- app/components/AppText.js
+++ app/components/AppText.js
@@ -1,22 +1,11 @@
 import React from "react";
-import { Text, StyleSheet, Platform } from "react-native";
+import { Text} from "react-native";
+
+import defaultStyles from "../config/styles";
 
 function AppText({ children, style }) {
-  return <Text style={[styles.text, style]}>{children}</Text>;
+  return <Text style={[defaultStyles.text, style]}>{children}</Text>;
 }
 
-// the select method returns an object, so i have to spread that object to copy its properties into the text object.
-const styles = StyleSheet.create({
-  text: {
-    color: "black",
-    ...Platform.select({
-      ios: { fontSize: 20, fontFamily: "American Typewriter" },
-      android: {
-        fontSize: 18,
-        fontFamily: "Roboto",
-      },
-    }),
-  },
-});
 
 export default AppText;
