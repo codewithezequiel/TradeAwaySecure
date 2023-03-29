98ef6f75d600086c7407a1187b61d216b447921e
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-29 11:20:39 -0700

    Work w switch comp w a state variable that changes.

--- App.js
+++ App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
+import { Switch } from "react-native";
 
 import Screen from "./app/components/Screen";
-import Icon from "./app/components/Icon";
-import ListItem from "./app/components/ListItem";
-import AccountScreen from "./app/screens/AccountScreen";
-import Footer from "./app/screens/Footer";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ListingScreens from "./app/screens/ListingScreens";
 
 export default function App() {
-  return <ListingScreens />;
+  const [isNew, setIsNew] = useState(false);
+  return (
+    <Screen>
+      <Switch value={isNew} onValueChange={(newValue) => setIsNew(newValue)} />
+    </Screen>
+  );
 }
