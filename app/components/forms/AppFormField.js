ec33340651a2b9a6e14d5633edf89dfcf3274217
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-30 05:34:17 -0700

    Add dynamic width to input components

--- app/components/forms/AppFormField.js
+++ app/components/forms/AppFormField.js
@@ -4,7 +4,7 @@ import { useFormikContext } from "formik";
 import AppTextInput from "../AppTextInput";
 import ErrorMessage from "./ErrorMessage";
 
-function AppFormField({ name, ...otherProps }) {
+function AppFormField({ name, width, ...otherProps }) {
   // Accessing these objects by using react context to pass objects down bc Formik can do this.
   const { setFieldTouched, handleChange, errors, touched } = useFormikContext();
   return (
@@ -12,6 +12,7 @@ function AppFormField({ name, ...otherProps }) {
       <AppTextInput
         onBlur={() => setFieldTouched(name)}
         onChangeText={handleChange(name)}
+        width={width}
         {...otherProps}
       />
       <ErrorMessage error={errors[name]} visible={touched[name]} />
