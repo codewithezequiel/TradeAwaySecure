0208921080a7c4cbba95bc31aa3885901da24d79
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-03-31 14:09:00 -0700

    Add some automatic scrolling, used ScrollView & useRef method.

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -1,24 +1,34 @@
 // this component allows users to select multipe images
 
-import React from "react";
-import { View, StyleSheet } from "react-native";
+import React, { useRef } from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
 import ImageInput from "./ImageInput";
 
 // for props: this interface is a collection of img inputs so we should have imgUris which we can set to
 // an empty array by default and then map it to imginput components
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  const scrollView = useRef();
+
   return (
-    <View style={styles.container}>
-      {imageUris.map((uri) => (
-        <View key={uri} style={styles.image}>
-          <ImageInput
-            imageUri={uri}
-            key={uri}
-            onChangeImage={() => onRemoveImage(uri)}
-          />
+    <View>
+      <ScrollView
+        ref={scrollView}
+        horizontal
+        onContentSizeChange={() => scrollView.current.scrollToEnd()}
+      >
+        <View style={styles.container}>
+          {imageUris.map((uri) => (
+            <View key={uri} style={styles.image}>
+              <ImageInput
+                imageUri={uri}
+                key={uri}
+                onChangeImage={() => onRemoveImage(uri)}
+              />
+            </View>
+          ))}
+          <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
         </View>
-      ))}
-      <ImageInput onChangeImage={(uri) => onAddImage(uri)} />
+      </ScrollView>
     </View>
   );
 }
