2f143738ed4e6ab02ed50ecbabb35c9715145a2e
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-04-03 07:40:59 -0700

    Work w react navigation, use a custom hook useNavigation() to child comp.

--- App.js
+++ App.js
@@ -1,8 +1,53 @@
-import React, { useState } from "react";
+import React from "react";
+import { Text } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
+import { Button } from "react-native";
+
 import Screen from "./app/components/Screen";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+function Link() {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+}
+
+function Tweets({ navigation }) {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Link />
+    </Screen>
+  );
+}
+
+function TweetDetails() {
+  return (
+    <Screen>
+      <Text>Tweet Details</Text>
+    </Screen>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function StackNavigator() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetDetails" component={TweetDetails} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+    // <Screen>
+    //   <Text>Hello</Text>
+    // </Screen>
+  );
 }
