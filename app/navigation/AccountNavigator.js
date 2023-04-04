e2a605e49feff9383d43124a13c7b8fbd08334ef
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-04-04 06:24:16 -0700

    nested components with same name, change name.

--- app/navigation/AccountNavigator.js
+++ app/navigation/AccountNavigator.js
@@ -9,7 +9,7 @@ const Stack = createStackNavigator();
 function AccountNavigator() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="Account" component={AccountScreen}></Stack.Screen>
+      <Stack.Screen name="Profile" component={AccountScreen}></Stack.Screen>
       <Stack.Screen name="Messages" component={MessagesScreen}></Stack.Screen>
     </Stack.Navigator>
   );
