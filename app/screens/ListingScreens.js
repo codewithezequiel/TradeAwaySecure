44a08fe9aa4a89beab9036a2f452ee6bae1613b6
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-04-04 06:17:29 -0700

    implement fetch data (images).

--- app/screens/ListingScreens.js
+++ app/screens/ListingScreens.js
@@ -1,29 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList, StyleSheet } from "react-native";
 
 import Card from "../components/Card";
 import Screen from "../components/Screen";
 import colors from "../config/colors";
+import listingsApi from "../api/listings";
 import routes from "../navigation/routes";
 
 function ListingScreens({ navigation }) {
   //temporary solution: array of objects
   // when we connect the app the backend, I am going to retrieve the listings
   // from the server
-  const listings = [
-    {
-      id: 1,
-      title: "Red jacket for sale",
-      price: 100,
-      image: require("../assets/jacket.jpg"),
-    },
-    {
-      id: 2,
-      title: "Couch in great condition",
-      price: 1001,
-      image: require("../assets/couch.jpg"),
-    },
-  ];
+
+  // we should declare a state variable to store the listings that we get from the server.
+  const [listings, setListings] = useState([]);
+
+  // now we should call our api the first time our component gets rendered. so we use the effect hook.
+  useEffect(() => {
+    loadListings();
+  }, []);
+
+  const loadListings = async () => {
+    const response = await listingsApi.getListings();
+    setListings(response.data);
+  };
 
   return (
     <Screen style={styles.screen}>
@@ -34,7 +34,7 @@ function ListingScreens({ navigation }) {
           <Card
             title={item.title}
             subTitle={"$" + item.price}
-            image={item.image}
+            imageUrl={item.images[0].url}
             onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
           />
         )}
