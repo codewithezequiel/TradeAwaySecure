d998f792ddd65051ef290058844327766b688a9a
Ezequiel Novoa <ezequiel.novoa3@gmail.com>
2023-04-11 23:07:57 -0700

    modified files.

--- app/components/forms/index.js
+++ app/components/forms/index.js
@@ -1,5 +1,5 @@
 export { default as Form } from "./Form";
-export { default as AppFormField } from "./FormField";
+export { default as FormField } from "./FormField";
 export { default as ErrorMessage } from "./ErrorMessage";
 export { default as SubmitButton } from "./SubmitButton";
 export { default as AppFormPicker } from "./FormPicker";
